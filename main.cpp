5f7dd51ee8a68033f24c702d749d0cf1663a53ad
ansar.hun@gmail.com <Kocsi Balazs>
2017-08-03 15:22:14 +0200

    use port option

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
 
 #include <QDebug>
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
+    app.setApplicationVersion("0.1.0");
 
-    qDebug() << "Hello workshop";
+    QCommandLineOption portOption {
+            QStringList {"p", "port"},
+            "Use this port to accept incoming connections",
+            "bind-port",
+            "3000"
+    };
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addOption(portOption);
+
+    parser.process(app);
+
+    qDebug() << "Hello workshop" << parser.value(portOption);
 
     return app.exec();
 }
